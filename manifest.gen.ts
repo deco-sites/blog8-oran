084dbb7a07044fa36931502c0bc2bd1163454a0a
decobot <nuo.ai@outlook.com>
2024-10-01 12:06:45 +0000

    Publish 9 from staging into origin/main

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,9 +2,13 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$$$$$$$$$0 from "./apps/deco/blog.ts";
-import * as $$$$$$$$$$$1 from "./apps/deco/htmx.ts";
-import * as $$$$$$$$$$$2 from "./apps/site.ts";
+import * as $$$$$$$$$$$0 from "./apps/deco/ai-assistants.ts";
+import * as $$$$$$$$$$$1 from "./apps/deco/blog.ts";
+import * as $$$$$$$$$$$2 from "./apps/deco/brand-assistant.ts";
+import * as $$$$$$$$$$$3 from "./apps/deco/htmx.ts";
+import * as $$$$$$$$$$$4 from "./apps/deco/records.ts";
+import * as $$$$$$$$$$$5 from "./apps/deco/workflows.ts";
+import * as $$$$$$$$$$$6 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/BlogPost.tsx";
 import * as $$$$$$1 from "./sections/BlogPosts.tsx";
 import * as $$$$$$2 from "./sections/Faq.tsx";
@@ -32,9 +36,13 @@ const manifest = {
     "site/sections/Theme/Theme.tsx": $$$$$$10,
   },
   "apps": {
-    "site/apps/deco/blog.ts": $$$$$$$$$$$0,
-    "site/apps/deco/htmx.ts": $$$$$$$$$$$1,
-    "site/apps/site.ts": $$$$$$$$$$$2,
+    "site/apps/deco/ai-assistants.ts": $$$$$$$$$$$0,
+    "site/apps/deco/blog.ts": $$$$$$$$$$$1,
+    "site/apps/deco/brand-assistant.ts": $$$$$$$$$$$2,
+    "site/apps/deco/htmx.ts": $$$$$$$$$$$3,
+    "site/apps/deco/records.ts": $$$$$$$$$$$4,
+    "site/apps/deco/workflows.ts": $$$$$$$$$$$5,
+    "site/apps/site.ts": $$$$$$$$$$$6,
   },
   "name": "site",
   "baseUrl": import.meta.url,
